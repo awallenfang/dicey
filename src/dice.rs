f14541d7d29f9bcaa50a08f89aaa388da96413da
RitzGit <marvin@wallenfang.de>
2021-08-24 19:37:59 +0200

    Added dice count

--- src/dice.rs
+++ src/dice.rs
@@ -2,20 +2,31 @@ use rand::Rng;
 
 pub struct Dice {
     eyes: i32,
+    count: i32,
 }
 
 impl Dice {
-    pub fn new(eyes: i32) -> Result<Dice, &'static str>{
+    pub fn new(eyes: i32) -> Result<Dice, &'static str> {
         if eyes > 0 {
-            Ok(Dice{eyes})
+            Ok(Dice {
+                eyes: eyes,
+                count: 1,
+            })
         } else {
             Err("Invalid Eye Count")
         }
-        
+    }
+    pub fn new_counted(eyes: i32, count: i32) -> Result<Dice, &'static str> {
+        if eyes == 0 {
+            return Err("Invalid Eye Count");
+        }
+        if count == 0 {
+            return Err("Invalid Dice Count");
+        }
+        Ok(Dice { eyes, count })
     }
     pub fn roll(&self) -> i32 {
-        
         let mut rng = rand::thread_rng();
-        rng.gen_range(1..=self.eyes)
+        self.count * rng.gen_range(1..=self.eyes)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,25 @@ mod tests {
     fn basic_dice() {
         let d6 = Dice::new(6).unwrap();
         let num = d6.roll();
-        assert!(num <= 6 && num >= 1);
+        assert!((1..=6).contains(&num));
     }
 
     #[test]
     fn invalid_eyes() {
         let d = Dice::new(0);
+        assert!(d.is_err());
+    }
+
+    #[test]
+    fn basic_counted_dice() {
+        let d6 = Dice::new_counted(6, 1).unwrap();
+        let num = d6.roll();
+        assert!((1..=6).contains(&num));
+    }
+
+    #[test]
+    fn invalid_count() {
+        let d = Dice::new_counted(20, 0);
         assert!(d.is_err());
     }
 }
