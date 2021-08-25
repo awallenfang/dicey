1eda714133559fee10426bb3047554eca40b8536
RitzGit <marvin@wallenfang.de>
2021-08-25 14:28:41 +0200

    Implemented Regex for dice parsing

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # TODO
 - [] Errors for failed die
+- [] Errors for failed parsing
 - [] Exploding die
 - [] Dropping die
-- [] Parsing
-- [] Errors for failed parsing
-- [] num.cmp for +/- Display
+- [] Parsing into DiceSet
+- [x] num.cmp for +/- Display
+- [] DiceSet for rolls like 1d10+2d6
 
 # Feature Plans
 - [] Configurable dice notation syntax
--- src/dice.rs
+++ src/dice.rs
@@ -1,10 +1,28 @@
+use lazy_static::lazy_static;
 use rand::Rng;
+use regex::Regex;
+use std::cmp::Ordering;
 use std::fmt;
+use std::num::ParseIntError;
+use std::str::FromStr;
 
+lazy_static! {
+    //FIND_DICE is currently not used
+    //TODO: Find a regex way to find the dice strings, instead of just looking for ds
+    static ref FIND_DICE: Regex = Regex::new(r"(?ix)\d*d").expect("Failed parsing FIND_DIE");
+    static ref DICE_CONTENT: Regex = Regex::new(r"(?ix)(?P<pre>[+-])?
+    (?P<count>\d+)?d
+    (?P<eyes>[\d%]+)
+    (?P<explode>!\d*)?
+    (?P<add>[+-]\d*)?").expect("Failed parsing DICE_CONTENT");
+}
+
+#[derive(PartialEq, Copy, Clone)]
 pub struct Dice {
     eyes: u16,
     count: u16,
     add: i32,
+    neg: bool,
 }
 
 impl Dice {
@@ -16,6 +34,7 @@ impl Dice {
             eyes,
             count: 1,
             add: 0,
+            neg: false,
         })
     }
     pub fn new_counted(eyes: u16, count: u16) -> Result<Dice, &'static str> {
@@ -29,6 +48,7 @@ impl Dice {
             eyes,
             count,
             add: 0,
+            neg: false,
         })
     }
     pub fn new_added(eyes: u16, add: i32) -> Result<Dice, &'static str> {
@@ -39,6 +59,7 @@ impl Dice {
             eyes,
             count: 1,
             add,
+            neg: false,
         })
     }
     pub fn new_subbed(eyes: u16, sub: i32) -> Result<Dice, &'static str> {
@@ -49,6 +70,7 @@ impl Dice {
             eyes,
             count: 1,
             add: -sub,
+            neg: false,
         })
     }
     pub fn new_full(eyes: u16, count: u16, add: i32) -> Result<Dice, &'static str> {
@@ -58,7 +80,26 @@ impl Dice {
         if count == 0 {
             return Err("Invalid Dice Count");
         }
-        Ok(Dice { eyes, count, add })
+        Ok(Dice {
+            eyes,
+            count,
+            add,
+            neg: false,
+        })
+    }
+    fn new_internal(eyes: u16, count: u16, add: i32, neg: bool) -> Result<Dice, &'static str> {
+        if eyes == 0 {
+            return Err("Invalid Eye Count");
+        }
+        if count == 0 {
+            return Err("Invalid Dice Count");
+        }
+        Ok(Dice {
+            eyes,
+            count,
+            add,
+            neg,
+        })
     }
     pub fn roll(&self) -> i32 {
         let mut rng = rand::thread_rng();
@@ -67,82 +108,140 @@ impl Dice {
             num => num,
         }
     }
+    fn roll_with_prefix(&self) -> i32 {
+        unimplemented!("The roll to be used with DiceSets when several dice are calculated together")
+    }
 }
 
 impl fmt::Display for Dice {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let base = format!("{}d{}", self.count, self.eyes);
-        let added = match self.add {
-            0 => String::from(""),
-            1_i32..=i32::MAX => format!("+{}", self.add),
-            i32::MIN..=-1_i32 => format!("{}", self.add),
+        let added = match self.add.cmp(&0) {
+            Ordering::Equal => String::from(""),
+            Ordering::Greater => format!("+{}", self.add),
+            Ordering::Less => format!("{}", self.add),
         };
         write! {f, "{}{}", base, added}
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::dice::Dice;
-    #[test]
-    fn basic_dice() {
-        let d6 = Dice::new(6).unwrap();
-        let num = d6.roll();
-        assert!((1..=6).contains(&num));
+impl FromStr for Dice {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Dice, Self::Err> {
+        //TODO: Dice positions
+        //TODO: String slices with just the dice
+        //TODO: Prefix
+        unimplemented!("Implement dice parsing");
+    }
+}
+
+#[test]
+fn basic_dice() {
+    let d6 = Dice::new(6).unwrap();
+    let num = d6.roll();
+    assert!((1..=6).contains(&num));
+}
+
+#[test]
+fn invalid_eyes() {
+    let d = Dice::new(0);
+    assert!(d.is_err());
+}
+
+#[test]
+fn basic_counted_dice() {
+    let d6 = Dice::new_counted(6, 1).unwrap();
+    let num = d6.roll();
+    assert!((1..=6).contains(&num));
+}
+
+#[test]
+fn invalid_count() {
+    let d = Dice::new_counted(20, 0);
+    assert!(d.is_err());
+}
+
+#[test]
+fn basic_added_dice() {
+    let d20_plus_69 = Dice::new_added(20, 69).unwrap();
+    let num = d20_plus_69.roll();
+    assert!((21..=89).contains(&num));
+}
+
+#[test]
+fn basic_subbed_dice() {
+    let d20_minus_5 = Dice::new_subbed(20, 5).unwrap();
+    let num = d20_minus_5.roll();
+    assert!((1..=15).contains(&num));
+}
+
+#[test]
+fn negative_roll() {
+    let d6_minus_10 = Dice::new_subbed(6, 10).unwrap();
+    let num = d6_minus_10.roll();
+    assert_eq!(&num, &1_i32);
+}
+
+#[test]
+fn display() {
+    let d20 = Dice::new(20).unwrap();
+    assert_eq!(d20.to_string(), "1d20");
+
+    let d20_plus_1 = Dice::new_added(20, 1).unwrap();
+    assert_eq!(d20_plus_1.to_string(), "1d20+1");
+
+    let five_d20_plus_69 = Dice::new_full(20, 5, 69).unwrap();
+    assert_eq!(five_d20_plus_69.to_string(), "5d20+69");
+
+    let five_d20_minus_69 = Dice::new_full(20, 5, -69).unwrap();
+    assert_eq!(five_d20_minus_69.to_string(), "5d20-69");
+}
+
+#[test]
+fn regex_find_dice() {
+    let caps = FIND_DICE.find_iter("1d10+d69+420d69+1337");
+    let mut d1_start = 0;
+    let mut d2_start = 0;
+    let mut d3_start = 0;
+
+    for (i, find) in caps.enumerate() {
+        match i {
+            0 => d1_start = find.start(),
+            1 => d2_start = find.start(),
+            2 => d3_start = find.start(),
+            _ => panic!("Too many finds"),
+        }
     }
 
-    #[test]
-    fn invalid_eyes() {
-        let d = Dice::new(0);
-        assert!(d.is_err());
-    }
+    assert_eq!(d1_start, 0);
+    assert_eq!(d2_start, 5);
+    assert_eq!(d3_start, 9);
+}
 
-    #[test]
-    fn basic_counted_dice() {
-        let d6 = Dice::new_counted(6, 1).unwrap();
-        let num = d6.roll();
-        assert!((1..=6).contains(&num));
-    }
+#[test]
+fn regex_dice_content() {
+    let caps1 = DICE_CONTENT.captures("1d10").unwrap();
+    assert_eq!(caps1.name("count").unwrap().as_str(), "1");
+    assert_eq!(caps1.name("eyes").unwrap().as_str(), "10");
+    assert!(caps1.name("add").is_none());
+    assert!(caps1.name("pre").is_none());
 
-    #[test]
-    fn invalid_count() {
-        let d = Dice::new_counted(20, 0);
-        assert!(d.is_err());
-    }
+    let caps2 = DICE_CONTENT.captures("420d69+1337").unwrap();
+    assert_eq!(caps2.name("count").unwrap().as_str(), "420");
+    assert_eq!(caps2.name("eyes").unwrap().as_str(), "69");
+    assert_eq!(caps2.name("add").unwrap().as_str(), "+1337");
+    assert!(caps2.name("pre").is_none());
 
-    #[test]
-    fn basic_added_dice() {
-        let d20_plus_69 = Dice::new_added(20, 69).unwrap();
-        let num = d20_plus_69.roll();
-        assert!((21..=89).contains(&num));
-    }
+    let caps3 = DICE_CONTENT.captures("-420d69-1337").unwrap();
+    assert_eq!(caps3.name("count").unwrap().as_str(), "420");
+    assert_eq!(caps3.name("eyes").unwrap().as_str(), "69");
+    assert_eq!(caps3.name("add").unwrap().as_str(), "-1337");
+    assert_eq!(caps3.name("pre").unwrap().as_str(), "-");
 
-    #[test]
-    fn basic_subbed_dice() {
-        let d20_minus_5 = Dice::new_subbed(20, 5).unwrap();
-        let num = d20_minus_5.roll();
-        assert!((1..=15).contains(&num));
-    }
-
-    #[test]
-    fn negative_roll() {
-        let d6_minus_10 = Dice::new_subbed(6, 10).unwrap();
-        let num = d6_minus_10.roll();
-        assert_eq!(&num, &1_i32);
-    }
-
-    #[test]
-    fn display() {
-        let d20 = Dice::new(20).unwrap();
-        assert_eq!(d20.to_string(), "1d20");
-
-        let d20_plus_1 = Dice::new_added(20, 1).unwrap();
-        assert_eq!(d20_plus_1.to_string(), "1d20+1");
-
-        let five_d20_plus_69 = Dice::new_full(20, 5, 69).unwrap();
-        assert_eq!(five_d20_plus_69.to_string(), "5d20+69");
-
-        let five_d20_minus_69 = Dice::new_full(20, 5, -69).unwrap();
-        assert_eq!(five_d20_minus_69.to_string(), "5d20-69");
-    }
+    let caps4 = DICE_CONTENT.captures("d%").unwrap();
+    assert!(caps4.name("count").is_none());
+    assert_eq!(caps4.name("eyes").unwrap().as_str(), "%");
+    assert!(caps4.name("add").is_none());
+    assert!(caps4.name("pre").is_none());
 }
